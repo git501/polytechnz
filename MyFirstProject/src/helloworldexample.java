fe803c9fd040af1ea2f5ed4b0686ba5a6fcb6da5
git501 <uioyh@DESKTOP-S34QFT0>
2019-07-01 17:15:02 +1200

    changed the // message

--- MyFirstProject/src/helloworldexample.java
+++ MyFirstProject/src/helloworldexample.java
@@ -2,7 +2,7 @@
 public class helloworldexample {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// Output Hello World to the user
 		System.out.println("hello world");
 
 	}
